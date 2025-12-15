a3741589477b9b72116d45a903616696ddfbf313
logscore <lsreedercontact@gmail.com>
2025-12-14 22:01:47 -0700

    trying to fix the resource hogging for the frontend build process

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -2,24 +2,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
-// import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		viteCommonjs()
-		// visualizer({ filename: 'stats.html', open: true })
-	],
-	build: {
-		minify: 'terser',
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true,
-				pure_funcs: ['console.log', 'console.info']
-			}
-		}
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
